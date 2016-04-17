9f06da3a19a6fae9ce135a60b3dc0daf6ff43039
Steven Thomas <steven.thomas@shopify.com>
2016-04-16 22:05:43 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+# Middleman Gems
+gem 'middleman', '>= 4.0.0'
+gem 'middleman-autoprefixer'
+gem 'middleman-es6', github: 'stevenmichaelthomas/middleman-es6'
+gem 'middleman-gh-pages'
---
+++ config.rb
@@ -0,0 +1,32 @@
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+activate :autoprefixer
+activate :es6
+
+# General configuration
+
+###
+# Helpers
+###
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+# Build-specific configuration
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+end
